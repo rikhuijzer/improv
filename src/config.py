5c47b61eba367bdea5137040bb81a8e5f24f0b95
Rik <t.h.huijzer@student.tue.nl>
2018-11-23 14:40:51 +0100

    moved logic to train, evaluate functions

---
+++ src/config.py
@@ -0,0 +1,99 @@
+from typing import NamedTuple
+from pathlib import Path
+from src.utils import get_project_root
+
+
+Params = NamedTuple('Params', [
+    ('data_dir', Path),
+    ('bert_config_file', Path),
+    ('task_name', str),
+    ('vocab_file', Path),
+    ('output_dir', Path),
+    ('init_checkpoint', Path),  # Initial checkpoint (usually from a pre-trained BERT model).
+    ('do_lower_case', bool),  # should be True for uncased and False otherwise
+    ('max_seq_length', int),  # Total input sentence length after WordPiece tokenization. Truncated and padded to match.
+    ('do_train', bool),
+    ('do_eval', bool),
+    ('do_predict', bool),
+    ('train_batch_size', int),
+    ('eval_batch_size', int),
+    ('predict_batch_size', int),
+    ('learning_rate', float),
+    ('num_train_epochs', float),
+    ('warmup_proportion', float),
+    ('save_checkpoints_steps', int),  # how often to save the model checkpoint
+    ('iterations_per_loop', int),  # how many steps to make in each estimator call
+    ('use_tpu', bool),
+    ('tpu_name', str),  # Either the name used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470
+    ('tpu_zone', str),  # [Optional] GCE zone for Cloud TPU, if not specified automated detection is attempted
+    ('gcp_project', str),  # [Optional] Project name Cloud TPU, if not specified automated detection is attempted
+    ('master', str),  # [Optional] TensorFlow master URL
+    ('num_tpu_cores', int)  # Only used if use_tpu is True
+])
+
+
+def get_debug_params() -> Params:
+    """Parameters for lightweight BERT execution for debug purposes."""
+    task_name = 'askubuntu'
+    bert_model = 'uncased_L-12_H-768_A-12'
+    bert_pretrained_dir = Path.home() / 'Downloads' / bert_model
+    output_dir_name = 'debug'
+
+    return Params(
+        data_dir=get_project_root() / 'data' / task_name,
+        bert_config_file=bert_pretrained_dir / 'bert_config.json',
+        task_name=task_name,
+        vocab_file=bert_pretrained_dir / 'vocab.txt',
+        output_dir=get_project_root() / 'generated' / task_name / output_dir_name,
+        init_checkpoint=bert_pretrained_dir / 'bert_model.ckpt',
+        do_lower_case=bert_model.startswith('uncased'),
+        max_seq_length=128,
+        do_train=False,
+        do_eval=False,
+        do_predict=False,
+        train_batch_size=1,
+        eval_batch_size=8,
+        predict_batch_size=8,
+        learning_rate=2e-5,
+        num_train_epochs=3.0,
+        warmup_proportion=0.1,
+        save_checkpoints_steps=1000,
+        iterations_per_loop=1000,
+        use_tpu=False,
+        tpu_name='',  # is used as tpu_address
+        tpu_zone=None,
+        gcp_project=None,
+        master=None,
+        num_tpu_cores=8
+    )
+
+
+def get_params() -> Params:
+    """Parameters for running BERT. Defaults can be found on Github."""
+    return Params(
+        data_dir=None,
+        bert_config_file=None,
+        task_name=None,
+        vocab_file=None,
+        output_dir=None,
+        init_checkpoint=None,
+        do_lower_case=True,
+        max_seq_length=128,
+        do_train=False,
+        do_eval=False,
+        do_predict=False,
+        train_batch_size=32,
+        eval_batch_size=8,
+        predict_batch_size=8,
+        learning_rate=5e-5,
+        num_train_epochs=3.0,
+        warmup_proportion=0.1,
+        save_checkpoints_steps=1000,
+        iterations_per_loop=1000,
+        use_tpu=False,
+        tpu_name=None,
+        tpu_zone=None,
+        gcp_project=None,
+        master=None,
+        num_tpu_cores=8
+    )
--- src/my_classifier.py
+++ src/my_classifier.py
@@ -5,6 +5,94 @@ from src.run_classifier import DataProcessor, InputExample
 from typing import Iterable
 import csv
 from pathlib import Path
+from src.run_classifier import convert_examples_to_features
+from src.config import Params
+from datetime import datetime
+from src.run_classifier import input_fn_builder, convert_examples_to_features, model_fn_builder
+import tensorflow as tf
+from src.modeling import BertConfig
+
+
+def get_model_and_estimator(params: Params, processor):
+    train_examples = processor.get_train_examples(params.data_dir)
+    num_train_steps = int(len(train_examples) / params.train_batch_size * params.num_train_epochs)
+    num_warmup_steps = int(num_train_steps * params.warmup_proportion)
+
+    model_fn = model_fn_builder(
+        bert_config=BertConfig.from_json_file(str(params.bert_config_file)),
+        num_labels=len(processor.get_labels()),
+        init_checkpoint=str(params.init_checkpoint),
+        learning_rate=params.learning_rate,
+        num_train_steps=num_train_steps,
+        num_warmup_steps=num_warmup_steps,
+        use_tpu=params.use_tpu,
+        use_one_hot_embeddings=True)
+
+    tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(params.tpu_name)  # used to be TPU_ADDRESS
+    run_config = tf.contrib.tpu.RunConfig(
+        cluster=tpu_cluster_resolver,
+        model_dir=params.output_dir,
+        save_checkpoints_steps=params.save_checkpoints_steps,
+        tpu_config=tf.contrib.tpu.TPUConfig(
+            iterations_per_loop=params.iterations_per_loop,
+            num_shards=params.num_tpu_cores,
+            per_host_input_for_training=tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2))
+
+    # If TPU is not available, this will fall back to normal Estimator on CPU or GPU.
+    estimator = tf.contrib.tpu.TPUEstimator(
+        use_tpu=params.use_tpu,
+        model_fn=model_fn,
+        config=run_config,
+        train_batch_size=params.train_batch_size,
+        eval_batch_size=params.eval_batch_size)
+
+    return model_fn, estimator
+
+
+def train(params: Params, processor, tokenizer, estimator):
+    train_examples = processor.get_train_examples(params.data_dir)
+    num_train_steps = int(len(train_examples) / params.train_batch_size * params.num_train_epochs)
+    train_features = convert_examples_to_features(
+        train_examples, processor.get_labels(), params.max_seq_length, tokenizer)
+    print('***** Started training at {} *****'.format(datetime.now()))
+    print('  Num examples = {}'.format(len(train_examples)))
+    print('  Batch size = {}'.format(params.train_batch_size))
+    tf.logging.info("  Num steps = %d", num_train_steps)
+
+    # see run_classifier.convert_single_example for feature creation
+    train_input_fn = input_fn_builder(
+        features=train_features,
+        seq_length=params.max_seq_length,
+        is_training=True,
+        drop_remainder=True)
+    estimator.train(input_fn=train_input_fn, max_steps=num_train_steps)
+    print('***** Finished training at {} *****'.format(datetime.now()))
+
+
+def evaluate(params: Params, processor, tokenizer, estimator):
+    # Eval the model.
+    eval_examples = processor.get_dev_examples(params.data_dir)
+    eval_features = convert_examples_to_features(
+        eval_examples, processor.get_labels(), params.max_seq_length, tokenizer)
+    print('***** Started evaluation at {} *****'.format(datetime.now()))
+    print('  Num examples = {}'.format(len(eval_examples)))
+    print('  Batch size = {}'.format(params.eval_batch_size))
+    # Eval will be slightly WRONG on the TPU because it will truncate
+    # the last batch.
+    eval_steps = int(len(eval_examples) / params.eval_batch_size)
+    eval_input_fn = input_fn_builder(
+        features=eval_features,
+        seq_length=params.max_seq_length,
+        is_training=False,
+        drop_remainder=True)
+    result = estimator.evaluate(input_fn=eval_input_fn, steps=eval_steps)
+    print('***** Finished evaluation at {} *****'.format(datetime.now()))
+    output_eval_file = params.output_dir / 'eval_results.txt'
+    with tf.gfile.GFile(str(output_eval_file), "w") as writer:
+        print("***** Eval results *****")
+        for key in sorted(result.keys()):
+            print('  {} = {}'.format(key, str(result[key])))
+            writer.write("%s = %s\n" % (key, str(result[key])))
 
 
 def get_intents(data_dir: Path) -> Iterable[str]:
